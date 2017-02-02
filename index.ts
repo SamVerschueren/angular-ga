20235b468303f58a86680d833612cc8e38f2c85c
Sam Verschueren <sam.verschueren@gmail.com>
2017-02-02 08:57:10 +0100

    move source files to src directory

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { GoogleAnalyticsModule } from './src/ga.module';
-export { GoogleAnalyticsService } from './src/ga.service';
-export { GA_TOKEN } from './src/ga.token';
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { GoogleAnalyticsModule } from './lib/ga.module';
+export { GoogleAnalyticsService } from './lib/ga.service';
+export { GA_TOKEN } from './lib/ga.token';
